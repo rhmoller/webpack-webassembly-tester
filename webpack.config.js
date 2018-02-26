c26fce4facfbb5e904d8335c72e9670846491cb7
Rene Hangstrup Møller <rhmoller@gmail.com>
2018-02-26 19:45:03 +0100

    chore: take advantage of webpack 4 defaults

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,6 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "app.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.wasm$/,
-                type: "webassembly/experimental"
-            }
-        ]
-    },
     plugins: [
         new HtmlWebpackPlugin()
     ]
